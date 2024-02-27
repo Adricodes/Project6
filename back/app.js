c4b249585adb22b4694a08dde8ceb49c2083f286
Adriana Rangel <adri.rangel6@gmail.com>
2024-02-27 10:10:38 -0800

    commit save retrieve data

--- back/app.js
+++ back/app.js
@@ -1,9 +1,94 @@
-const express = require('express');
+//MONGO PWD
+//MONGO CONNECTION
 
-const app = express();n
+express = require('express');
+const mongoose = require('mongoose');
+const Thing = require('./models/thing');
 
-app.use((req, res) => {
-    res.json({ message: 'Your request was successful!' }); 
- });
+app.post('/api/stuff', (req, res, next) => {
+    const thing = new Thing({
+      title: req.body.title,
+      description: req.body.description,
+      imageUrl: req.body.imageUrl,
+      price: req.body.price,
+      userId: req.body.userId
+    });
+    thing.save().then(
+      () => {
+        res.status(201).json({
+          message: 'Post saved successfully!'
+        });
+      }
+    ).catch(
+      (error) => {
+        res.status(400).json({
+          error: error
+        });
+      }
+    );
+  });
+
+mongoose.connect(mongodb+srv:adrirangel6:<password>@cluster0.fvrfhjf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0)
+.then(()) => 
+{
+
+}
+app.use(express.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+app.use(bodyParser.json())
+
+
+app.post('/api/stuff', (req, res, next) => {
+  const thing = new Thing({
+    title: req.body.title,
+    description: req.body.description,
+    imageUrl: req.body.imageUrl,
+    price: req.body.price,
+    userId: req.body.userId
+  });
+  thing.save().then(
+    () => {
+      res.status(201).json({
+        message: 'Post saved successfully!'
+      });
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
+});
+
+app.get('/api/stuff', (req, res, next) => {
+    const stuff = [
+        {
+            _id: 'oeihfzeoi',
+            title: 'My first thing',
+            description: 'All of the info about my first thing',
+            imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+            price: 4900,
+            userId: 'qsomihvqios',
+        },
+        {
+            _id: 'oeihfzeomoihi',
+            title: 'My second thing',
+            description: 'All of the info about my second thing',
+            imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+            price: 2900,
+            userId: 'qsomihvqios',
+        },
+    ];
+    res.status(200).json(stuff);
+});
 
 module.exports = app;
+
